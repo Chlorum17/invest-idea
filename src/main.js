eb2a36cc8a0287b9f36a3523bf4468bfc0626858
Alexander Dremin <otakusama94@gmail.com>
2021-05-18 15:23:50 +0300

    feat: написал функцию, запускающую проект

---
+++ src/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+require('dotenv').config();
+
+const startServer = require('./server');
+
+const connectToMongoDB = require('./db-connection');
+
+const logger = require('../logger');
+
+async function bootstrap() {
+  try {
+    startServer(process.env.INVEST_IDEA_SERVER_PORT);
+    logger.info(
+      `Server started at port ${process.env.INVEST_IDEA_SERVER_PORT}`,
+    );
+
+    await connectToMongoDB(process.env.INVEST_IDEA_MONGO_URI);
+    logger.info(`Connected to MongoDB`);
+  } catch (error) {
+    logger.error(error);
+    process.exit(1);
+  }
+}
+
+bootstrap();
