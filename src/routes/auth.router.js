0006b0bf2d5e2e77b0095d2495f6cc720302305e
Alexander Dremin <otakusama94@gmail.com>
2021-05-19 09:03:51 +0300

    правка коммита

--- src/routes/auth.router.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const router = require('express').Router();
-
-const authController = require('../common/auth/auth.controller');
-
-router.post('/login', authController.login);
-
-module.exports = router;
--- src/routes/user.router.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const router = require('express').Router();
-
-const userController = require('../entities/user/user.controller');
-
-router.post('/register', userController.register);
-
-module.exports = router;
