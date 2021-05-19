3157dcc8b7384eb82dc7fa179c62e63bceb34774
Alexander Dremin <otakusama94@gmail.com>
2021-05-19 15:43:06 +0300

    ref: поправил user.model

--- src/entities/user/user.model.js
+++ src/entities/user/user.model.js
@@ -4,10 +4,9 @@ const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema(
   {
-    firstName: { type: String, required: true },
+    name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    ideasRating: [{ type: Schema.Types.ObjectId, ref: 'IdeaRating' }],
   },
   { timestamps: true },
 );
