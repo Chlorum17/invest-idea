bb90376005dd8274a69b9d9a04cd4685b9db6ea7
Alexander <58850665+Chlorum17@users.noreply.github.com>
2021-05-19 06:54:51 +0300

    merge:добавил функционал для invest-idea (#6)
    * feat: установил losash, api-query-params
    
    * feat: поправил idea-rating.model
    
    * feat: поправил invest-idea.model
    
    * feat: добавил invest-idea.controller
    
    * feat: добавил invest-idea.service
    
    * feat: добавил invest-idea.sanitize
    
    * feat: добавил пути для контроллера invest-idea

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,10 @@ const express = require('express');
 
 const router = express.Router();
 
+const investIdeaRouter = require('./invest-idea');
+
 router.use(express.json());
+
+router.use('/invest-ideas', investIdeaRouter);
 
 module.exports = router;
