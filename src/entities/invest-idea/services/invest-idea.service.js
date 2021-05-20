e4a958dc605b374d754d84e2cd441c9cc0fd84f4
Alexander <58850665+Chlorum17@users.noreply.github.com>
2021-05-20 15:54:39 +0300

    merge: добавил функционал income-chart (#12)
    * feat: добавил метод getIdeaIncomeChart в invest-idea.controller
    
    Добавил метод findByIdAndUpdate, тк необходимо было добавить записи с currentIncomeHistory
    Поменял аргумент вызова метода findById invest-idea.service с req.params._id, на req.params
    
    * feat: добавил поле currentIncomeHistory в invest-idea.model
    
    currentIncomeHistory: [
      {
        _id: false,
        averageValue: { type: Number, required: true },
        date: { type: Date, required: true },
      }
    ]
    
    * feat: добавил пути
    
    * feat: добавил функционал incomeChart
    
    Написал методы, для функционала incomeChart.
    Декомпозировал invest-idea.service, на invest-idea.service, invest-idea.sanitize, income-calc.service, income-chart.service.
    Декомпозировал invest-idea.sanitize.
    В папке invest-idea, создал папку по сервисы, вынес туда сервисы..

---
+++ src/entities/invest-idea/services/invest-idea.service.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const InvestIdeaModel = require('../invest-idea.model');
+
+const sanitize = require('./invest-idea.sanitize');
+const incomeCalc = require('./income-calc.service');
+const incomeChart = require('./income-chart.service');
+
+const service = {
+  async find({ filter, skip, limit, sort }) {
+    const investIdeas = await InvestIdeaModel.find(filter)
+      .skip(skip)
+      .limit(limit)
+      .sort(sort);
+    return investIdeas.map(sanitize.sanitizeIdeasList);
+  },
+
+  async findById({ _id }) {
+    const investIdea = await InvestIdeaModel.findById(_id);
+    return sanitize.sanitizeIdeaInDetail(investIdea);
+  },
+
+  async create(createIdeaDto) {
+    const currentIncome = incomeCalc.currentIncomeCalc(createIdeaDto);
+
+    const predictedIncome = incomeCalc.predictedIncomeCalc(createIdeaDto);
+
+    const ideaRealization = incomeCalc.ideaRealizationCalc(
+      predictedIncome,
+      currentIncome,
+    );
+    const investIdea = {
+      ...createIdeaDto,
+      predictedIncome,
+      currentIncome,
+      ideaRealization,
+    };
+
+    const existingInvestIdea = await InvestIdeaModel.create(investIdea);
+
+    return sanitize.sanitizeIdeaInDetail(existingInvestIdea);
+  },
+
+  async getIdeaIncomeChart({ ideaId }, { period }) {
+    const { startOfPeriod, endOfPeriod } = incomeChart.getPeriodLimits(period);
+
+    const { currentIncomeHistory } = await InvestIdeaModel.findById(ideaId);
+
+    const currentIncomeChart = currentIncomeHistory.filter(
+      (el) => el.date >= startOfPeriod && el.date <= endOfPeriod,
+    );
+    return currentIncomeChart;
+  },
+
+  async findByIdAndUpdate({ _id }, { averageValue, date }) {
+    const updatedIdea = await InvestIdeaModel.findOneAndUpdate(
+      { _id },
+      { $addToSet: { currentIncomeHistory: { averageValue, date } } },
+      {
+        new: true,
+      },
+    );
+    return updatedIdea;
+  },
+};
+
+module.exports = service;
