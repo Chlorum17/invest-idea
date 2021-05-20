c8ee943502980ce9d6bc05b74ddec3318be4999c
Alexander Dremin <otakusama94@gmail.com>
2021-05-20 15:42:48 +0300

    feat: добавил метод getIdeaIncomeChart в invest-idea.controller
    Добавил метод findByIdAndUpdate, тк необходимо было добавить записи с currentIncomeHistory
    Поменял аргумент вызова метода findById invest-idea.service с req.params._id, на req.params

--- src/entities/invest-idea/invest-idea.controller.js
+++ src/entities/invest-idea/invest-idea.controller.js
@@ -2,7 +2,7 @@
 
 const aqp = require('api-query-params');
 
-const investIdeaService = require('./invest-idea.service');
+const investIdeaService = require('./services/invest-idea.service');
 
 const controller = {
   async find(req, res) {
@@ -22,7 +22,7 @@ const controller = {
 
   async findById(req, res) {
     try {
-      const investIdea = await investIdeaService.findById(req.params._id);
+      const investIdea = await investIdeaService.findById(req.params);
 
       if (!investIdea)
         return res.status(404).json({ message: 'No Invest Idea found' });
@@ -39,6 +39,34 @@ const controller = {
     try {
       const investIdea = await investIdeaService.create(req.body);
       return res.status(201).json(investIdea);
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: `Internal server error: ${error}` });
+    }
+  },
+
+  async getIdeaIncomeChart(req, res) {
+    try {
+      const ideaIncomeChart = await investIdeaService.getIdeaIncomeChart(
+        req.params,
+        req.query,
+      );
+      return res.status(200).json(ideaIncomeChart);
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: `Internal server error: ${error}` });
+    }
+  },
+
+  async findByIdAndUpdate(req, res) {
+    try {
+      const updatedIvestIdea = await investIdeaService.findByIdAndUpdate(
+        req.params,
+        req.body,
+      );
+      return res.status(200).json(updatedIvestIdea);
     } catch (error) {
       return res
         .status(500)
