83a9d449bb3d123de3e1dd1de44e967514f36836
Alexander Dremin <otakusama94@gmail.com>
2021-05-25 22:09:26 +0300

    ref: переместил приватный метод вниз

--- src/entities/invest-idea/services/income-chart.service.js
+++ src/entities/invest-idea/services/income-chart.service.js
@@ -1,23 +1,6 @@
 'use strict';
 
 const incomeChartService = {
-  _getTimePeriodsAgo() {
-    const HOUR_IN_MILLISEC = 60 * 60 * 1000;
-    const DAY_IN_MILLISEC = HOUR_IN_MILLISEC * 24;
-    const WEEK_IN_MILLISEC = DAY_IN_MILLISEC * 7;
-
-    const MONTH_IN_MILLISEC = DAY_IN_MILLISEC * 30;
-    const YEARS_IN_MILLISEC = 365 * DAY_IN_MILLISEC;
-
-    return {
-      timeHourAgo: new Date(new Date() - HOUR_IN_MILLISEC),
-      timeDayAgo: new Date(new Date() - DAY_IN_MILLISEC),
-      timeWeekAgo: new Date(new Date() - WEEK_IN_MILLISEC),
-      timeMonthAgo: new Date(new Date() - MONTH_IN_MILLISEC),
-      timeYearAgo: new Date(new Date() - YEARS_IN_MILLISEC),
-    };
-  },
-
   getPeriodLimits(period) {
     const { timeHourAgo, timeDayAgo, timeWeekAgo, timeMonthAgo, timeYearAgo } =
       this._getTimePeriodsAgo();
@@ -46,6 +29,23 @@ const incomeChartService = {
     }
 
     return { startOfPeriod, endOfPeriod };
+  },
+
+  _getTimePeriodsAgo() {
+    const HOUR_IN_MILLISEC = 60 * 60 * 1000;
+    const DAY_IN_MILLISEC = HOUR_IN_MILLISEC * 24;
+    const WEEK_IN_MILLISEC = DAY_IN_MILLISEC * 7;
+
+    const MONTH_IN_MILLISEC = DAY_IN_MILLISEC * 30;
+    const YEARS_IN_MILLISEC = 365 * DAY_IN_MILLISEC;
+
+    return {
+      timeHourAgo: new Date(new Date() - HOUR_IN_MILLISEC),
+      timeDayAgo: new Date(new Date() - DAY_IN_MILLISEC),
+      timeWeekAgo: new Date(new Date() - WEEK_IN_MILLISEC),
+      timeMonthAgo: new Date(new Date() - MONTH_IN_MILLISEC),
+      timeYearAgo: new Date(new Date() - YEARS_IN_MILLISEC),
+    };
   },
 };
 
